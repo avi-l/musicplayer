d909368130a710875ffaa744fddc1b63eca47327
Avi Leeker <avi.leeker@gmail.com>
2022-05-31 09:21:35 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,11 @@
+React Native Music Player
+-------------------------
+
+### A Music Player for Android Devices
+
+![](assets/20220531_092007_Player.jpeg)
+
+![](assets/20220531_092021_Playlist.jpeg)
+
+
+
--- app/constants/constants.js
+++ app/constants/constants.js
@@ -3,10 +3,12 @@ export const colorScheme = {
     // lightest: '#ECE5C7',
     // dark: '#CDC2AE',
     // darkest: '#354259',
+    
     // darkest: '#000D6B',
     // dark: '#9C19E0',
     // light: '#FF5DA2',
     // lightest: '#99DDCC',
+
     darkest: '#3A3845',
     dark: '#826F66',
     light: '#C69B7B',
